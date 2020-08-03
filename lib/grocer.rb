2bf942bedbe52467a31650c98ed66f257474d281
Anson Nickel <anson.nickel@gmail.com>
2020-08-03 00:08:42 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,15 +1,5 @@
 require 'pry'
 
-test_cart = [
-  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
-  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
-  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
-  { :item => "SALSA", :price => 1.50, :clearance => false },
-  { :item => "SALSA", :price => 1.50, :clearance => false },
-  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
-  { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
-]
-
 def find_item_by_name_in_collection(name, collection)
   collection.find {|x| x[:item] == name}
 end
